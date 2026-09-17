503e6a38d5ec95679cb441e66c357ac84294adc6
Sarah Imhoff <imh0425@swtc.edu>
2026-09-17 10:29:22 -0500

    Style updates, trying to finess a bitt

--- week_5/05_Thursday_Assignment/script.js
+++ week_5/05_Thursday_Assignment/script.js
@@ -1,18 +1,3 @@
-const payrollBtn = document.querySelector("#payrollBtn");
-const payrollCountdown = document.querySelector("#payrollCountdown");
-
-function DaysUntilPayroll() {
-    const today = new Date();
-    const payrollDate = new Date("2026-09-25");
-    const millisecondsperday = 1000 * 60 * 60 * 24;
-    const DaysUntilPayroll = Math.ceil((payrollDate - today) / millisecondsperday);
-    return DaysUntilPayroll;
-}
-
-payrollBtn.addEventListener("click", function() {
-    payrollCountdown.textContent = "Days until next payroll: " + DaysUntilPayroll();
-});
-
 const hoursWorked = document.querySelector("#hoursWorked");
 const hourlyRate = document.querySelector("#hourlyRate");
 const grosspayBtn = document.querySelector("#grosspayBtn");
@@ -32,3 +17,18 @@ function calculateGrossPay() {
 }
 
 grosspayBtn.addEventListener("click", calculateGrossPay);
+
+const payrollBtn = document.querySelector("#payrollBtn");
+const payrollCountdown = document.querySelector("#payrollCountdown");
+
+function DaysUntilPayroll() {
+    const today = new Date();
+    const payrollDate = new Date("2026-09-25");
+    const millisecondsperday = 1000 * 60 * 60 * 24;
+    const DaysUntilPayroll = Math.ceil((payrollDate - today) / millisecondsperday);
+    return DaysUntilPayroll;
+}
+
+payrollBtn.addEventListener("click", function() {
+    payrollCountdown.textContent = "Days until next payroll: " + DaysUntilPayroll();
+})
